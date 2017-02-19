3ab4a2d833e3b1cf9bd2605d31011e1670aad8fc
Khemtat Lengpaiboon <khemtat.966@gmail.com>
2017-02-20 01:33:57 +0700

    Hotfix app.js for conflict problem

--- app.js
+++ app.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-=======
 const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
@@ -66,13 +32,11 @@ app.use(function(req, res, next) {
   var err = new Error('Not Found')
   err.status = 404
   next(err)
->>>>>>> dev
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-<<<<<<< HEAD
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
@@ -81,17 +45,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
-=======
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
-
 app.listen(8080, () => {
   console.log('Listening on localhost:8080...')
 })
->>>>>>> dev
