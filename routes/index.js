633c7abaff4b586ff494468ee9e7635d9ac62765
kittikorn <s6070006@kmitl.ac.th>
2017-02-20 22:16:08 +0700

    Remove Head conflict in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
-=======
 const express = require('express')
 const router = express.Router()
 
