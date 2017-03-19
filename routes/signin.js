ca77948ca09878cb33a9d7272355988f2b9493e7
Khemtat Lengpaiboon <khemtat.966@gmail.com>
2017-03-19 18:48:52 +0700

    Merge login into signin

--- routes/signin.js
+++ routes/signin.js
@@ -3,12 +3,6 @@ const router = express.Router()
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD:routes/signin.js
   res.render('signin')
 });
-=======
-  res.render('login')
-})
->>>>>>> 851bd3d85d8dfd540b83d26c54dabd7ab6a43268:routes/login.js
-
 module.exports = router
