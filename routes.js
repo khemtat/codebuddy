759175d2ec8a65981301c952067841e32db8ca08
Khemtat Lengpaiboon <khemtat.966@gmail.com>
2017-04-04 02:34:10 +0700

    Move all routes required to routes.js file

---
+++ routes.js
@@ -0,0 +1,11 @@
+const index = require('./routes/index')
+const users = require('./routes/users')
+const dashboard = require('./routes/dashboard')
+const register = require('./routes/register')
+const signin = require('./routes/signin')
+
+exports.index = index
+exports.users = users
+exports.dashboard = dashboard
+exports.register = register
+exports.signin = signin
