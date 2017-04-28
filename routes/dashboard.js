e1becb6af48983fb5e9ca42264287e4a66cfa9df
Khemtat Lengpaiboon <khemtat.966@gmail.com>
2017-04-28 23:31:58 +0700

    Delete unnecessary files

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -13,7 +13,7 @@ const router = express.Router()
  * Finding user projects from database and pass results to the dashboard file
  * @method {GET} return rendered `dashboard.pug`
  */
-router.get('/dashboard', auth.isSignedIn, (req, res) => {
+router.get('/', auth.isSignedIn, (req, res) => {
   const query = [{ creator: req.user.username }, { collaborator: req.user.username }]
   Project.find({ $or: query }, (err, docs) => {
     res.render('dashboard', { user: req.user, projects: docs })
