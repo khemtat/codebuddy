bba119ef632ce8283f9cf0e124d3c548092bfea0
Khemtat Lengpaiboon <khemtat.966@gmail.com>
2017-05-30 14:43:19 +0700

    User can edit his profile

--- routes/settings.js
+++ routes/settings.js
@@ -2,18 +2,19 @@
  * Module dependencies
  */
 const express = require('express')
+const mongoose = require('mongoose')
 
+const User = mongoose.model('User')
 const router = express.Router()
 
-/**
- * `Settings`
- * @method {GET}
- * @method {POST}
- */
 router.route('/profile')
   .get((req, res) => {
-    res.render('editprofile')
+    res.render('editprofile', { user: req.user, title: 'Profile Settings' })
     // TODO implementation here
+  })
+  .post((req, res) => {
+    // TODO implement here
+    res.send('OK')
   })
 
 /**
